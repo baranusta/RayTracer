adf7cd2d68f202f08976beeea180c0d4ca53f880
baran usta <baranusta@sabanciuniv.edu>
2015-10-03 18:00:33 +0300

    tlogs removed and test class added

---
+++ RayTracerCUDA/Test/MainTest.cpp
@@ -0,0 +1,23 @@
+#define CATCH_CONFIG_RUNNER  // This tells Catch to provide a main() - only do this in one cpp file
+#include "Catch/catch.h"
+
+int TestMain()
+{
+
+	int result = Catch::Session().run();
+
+	// global clean-up...
+
+	return result;
+}
+
+unsigned int Factorial(unsigned int number) {
+	return number <= 1 ? number : Factorial(number - 1)*number;
+}
+
+TEST_CASE("Factorials are computed", "[factorial]") {
+	REQUIRE(Factorial(1) == 1);
+	REQUIRE(Factorial(2) == 2);
+	REQUIRE(Factorial(3) == 6);
+	REQUIRE(Factorial(10) == 3628800);
+}
